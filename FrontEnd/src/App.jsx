e31a7501b249190bf69f8bdba852fe15ae462c14
Sébastien <seb.juillet987@hotmail.fr>
2024-05-09 21:49:18 +0200

    create NavBar, Footer and Home page

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
+import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <AppRoutes />
       <Footer />
     </BrowserRouter>
