9b67e58850e668e18b359963b375c5f12fda3978
Sébastien <seb.juillet987@hotmail.fr>
2024-05-24 11:38:55 +0200

    removing unnecessary code in connectslice

--- FrontEnd/src/redux/Slice/connectSlice.js
+++ FrontEnd/src/redux/Slice/connectSlice.js
@@ -16,7 +16,7 @@ export const signInSlice = createSlice({
             state.token = null
             state.isLoggedIn = false
             state.loading = false
-            state.error = false
+            state.error = null
             sessionStorage.clear()
         }
     },
@@ -49,52 +49,3 @@ export const signInSlice = createSlice({
 export const {signOut} = signInSlice.actions
 export default signInSlice.reducer
 
-// import { createSlice } from "@reduxjs/toolkit"
-// import { auth } from "../../services/callAPI"
-
-// const initialState = {
-//     // user: null,
-//     token: null,
-//     isLoggedIn: false,
-//     error: null,
-// }
-
-// export const signInSlice = createSlice({
-//     name: "signIn",
-//     initialState,
-//     reducers: {
-//         signOut: (state) => {
-//             // state.user = null
-//             state.token = null
-//             state.isLoggedIn = false
-//             state.error = false
-//             // localStorage.clear()
-//         }
-//     },
-//     extraReducers: (builder) => {
-//         builder
-//         .addCase(auth.fulfilled, (state, action) => {
-//             // state.user = action.payload
-//             state.token = action.payload.token
-//             state.isLoggedIn = true
-//             state.error = null
-//             // localStorage.setItem("token", action.payload.token)
-//         })
-//         .addCase(auth.pending, (state) => {
-//             // state.user = null
-//             state.token = null
-//             state.isLoggedIn = true
-//             state.error = null
-//         })
-//         .addCase(auth.rejected, (state, action) => {
-//             // state.user = null
-//             state.token = null
-//             state.isLoggedIn = false
-//             state.error = action.error.message
-//         })
-// }
-// })
-
-
-// export const {signOut} = signInSlice.actions
-// export default signInSlice.reducer
